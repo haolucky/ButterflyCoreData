df009f63315801eeddd1570194547bb48c8b85e1
haolucky <hao.lucky@gmail.com>
2020-08-21 12:17:27 +1000

    add core date entities and relationships
    BFProductionOrder, BFItems, BFInvoice, and BFReceipt

---
+++ Butterfly-CoreData/controllers/BFTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  BFTableViewController.swift
+//  Butterfly-CoreData
+//
+//  Created by Chris on 2020/8/21.
+//  Copyright © 2020 Butterfly. All rights reserved.
+//
+
+import UIKit
+
+class BFTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
